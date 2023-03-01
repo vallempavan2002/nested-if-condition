5536a9ece752597b1a1851d919bf17702f9b3844
Pavan kalyan Vallem <116752882+vallempavan2002@users.noreply.github.com>
2023-03-01 15:11:52 +0530

    Create nested if condition.java

---
+++ nested if condition.java
@@ -0,0 +1,21 @@
+/*nested if statement  with 
+code a if statement for blood donation
+
+*/
+public class Main
+{
+	public static void main(String[] args){ 
+	int age=18;
+	int weight=50;
+	{
+	    if (age>18){
+	        if (weight>50)
+		System.out.println("eligible for blood donation");
+	}
+	else{
+	    System.out.println("not eligible");
+	}
+	}
+}
+}
+
